10902f94c4417b1c1c2f0f93d66ed6b210d03cf4
Brian <briantcheung@gmail.com>
2024-06-11 19:53:33 -0700

    Added basic homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,33 @@
 import * as React from "react"
+import { useTheme } from "@mui/material/styles"
+
+// MUI imports
+import { Box, Typography } from "@mui/material"
 
 const Home = () => {
 
+    const theme = useTheme()
+
     return (
         <React.Fragment>
-            Project Tacetite
+            <Box>
+                <Box
+                    sx={{
+                        backgroundColor: `${theme.paper.backgroundColor}`,
+                        border: `1px solid ${theme.border.color}`,
+                        borderRadius: "5px",
+                        display: "block",
+                        margin: "auto",
+                        mt: "20px",
+                        width: "70%",
+                        p: "20px",
+                        textAlign: "center",
+                        color: `${theme.text.color}`,
+                    }}
+                >
+                    <Typography variant="h3" component="p">Welcome to PROJECT TACETITE</Typography>
+                </Box>
+            </Box>
         </React.Fragment>
     )
 
