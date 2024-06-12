6dcb85f7d04063a450d0131b24050de92fb37f81
Brian <briantcheung@gmail.com>
2024-06-12 00:54:32 -0700

    Added basic character cards

--- src/components/characters/CharacterBrowser.tsx
+++ src/components/characters/CharacterBrowser.tsx
@@ -1,24 +1,63 @@
 import * as React from "react"
-import { connect } from "react-redux";
+import { useTheme } from "@mui/material/styles"
+import { connect } from "react-redux"
+
+// Component imports
+import CharacterCard from "./CharacterCard"
+
+// MUI imports
+import { Box, Typography, Paper, InputBase, Stack, ToggleButtonGroup } from "@mui/material"
+import Grid from "@mui/material/Unstable_Grid2"
 
 // Type imports
 import { RootState } from "../../redux/store"
 
 const CharacterBrowser = (props: any) => {
 
+    const theme = useTheme();
+
     let { characters } = props;
 
+    document.title = "Characters - Project Tacetite"
+
     return (
-        <>
-            {
-                characters.characters.length > 0 &&
-                <ul>
-                    {
-                        characters.characters.map((char: { [key: string]: any }) => <li key={char.id}>{char.name}</li>)
-                    }
-                </ul>
-            }
-        </>
+        <React.Fragment>
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "left",
+                }}
+            >
+                <Typography variant="h4"
+                    sx={{
+                        mx: "25px",
+                        my: "20px",
+                        display: { xs: "none", md: "flex" },
+                        letterSpacing: ".2rem",
+                        color: `${theme.text.color}`,
+                        textDecoration: "none",
+                        textAlign: "center",
+                    }}
+                >
+                    RESONATORS
+                </Typography>
+            </Box>
+            <Grid container sx={{ margin: "auto", width: "98%" }}>
+                <Grid xs={9}>
+                    <Grid container>
+                        {characters.characters.length > 0 &&
+                            <React.Fragment>
+                                {
+                                    characters.characters.map((char: { [key: string]: any }) => <CharacterCard key={char.id} character={char} />)
+
+                                }
+                            </React.Fragment>
+                        }
+                    </Grid>
+                </Grid>
+            </Grid>
+        </React.Fragment>
     )
 
 }
---
+++ src/components/characters/CharacterCard.tsx
@@ -0,0 +1,121 @@
+import { useTheme } from "@mui/material/styles"
+
+// Component imports
+import CharacterMaterialGrid from "./CharacterMaterialGrid"
+
+// MUI imports
+import { Typography, Card, CardContent, ButtonBase, Box } from "@mui/material"
+import Grid from "@mui/material/Unstable_Grid2"
+
+// Helper imports
+import { CustomTooltip } from "../../helpers/CustomTooltip"
+import ErrorLoadingImage from "../../helpers/ErrorLoadingImage"
+
+const CharacterCard = (props: any) => {
+
+    const theme = useTheme()
+
+    let { name, rarity, element, weapon } = props.character
+
+    const characterIconBackground = {
+        margin: "auto",
+        marginLeft: "2px",
+        backgroundColor: `${theme.materialImage.backgroundColor}`,
+        backgroundSize: "100%",
+        border: `1px solid ${theme.border.color}`,
+        borderRadius: "15px",
+        width: "100px",
+        height: "100px",
+        borderBottom: `8px solid ${GetRarityColor(rarity)}`,
+        boxShadow: `inset 0 0 30px ${GetBackgroundColor(rarity)}`,
+    }
+
+    return (
+        <Card variant="outlined"
+            sx={{
+                width: 320,
+                height: 195,
+                mr: "15px",
+                mb: "15px",
+                backgroundColor: `${theme.card.backgroundColor}`,
+                border: `1px solid ${theme.border.color}`,
+                borderRadius: "25px 25px 50px 25px",
+            }}
+        >
+            <CardContent sx={{ py: "10px" }}>
+                <Box
+                    sx={{
+                        display: "flex",
+                        position: "relative"
+                    }}
+                >
+                    <ButtonBase disableRipple href={`/project-tacetite/character/${props.character.name.split(" ").join("_").toLowerCase()}`} target="_blank">
+                        <Typography variant="h5" sx={{ color: `${theme.text.color}`, fontWeight: "500" }}>
+                            {name}
+                        </Typography>
+                    </ButtonBase>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            position: "absolute",
+                            right: "-5px"
+                        }}
+                    >
+                        <CustomTooltip title={element} arrow placement="top">
+                            <img style={{ height: "36px", width: "36px", marginRight: "1px" }} src={(`${process.env.REACT_APP_URL}/elements/${element}.png`)} alt={element} onError={ErrorLoadingImage} />
+                        </CustomTooltip>
+                        <CustomTooltip title={weapon} arrow placement="top">
+                            <img style={{ height: "36px", width: "36px" }} src={(`${process.env.REACT_APP_URL}/weapons/icons/${weapon}.png`)} alt={weapon} onError={ErrorLoadingImage} />
+                        </CustomTooltip>
+                    </Box>
+                </Box>
+                <Grid container sx={{ mt: "10px" }}>
+                    <Grid xs>
+                        <Box sx={{ width: "105px" }}>
+                            <ButtonBase disableRipple href={`/project-tacetite/character/${props.character.name.split(" ").join("_").toLowerCase()}`} target="_blank">
+                                <img src={`${process.env.REACT_APP_URL}/characters/icons/${name}.png`} alt={name} style={characterIconBackground} onError={ErrorLoadingImage} />
+                            </ButtonBase>
+                            <img src={(`${process.env.REACT_APP_URL}/stars/${rarity}Star.png`)} alt={rarity}
+                                style={{
+                                    display: "block",
+                                    margin: "auto",
+                                    marginTop: "5px",
+                                    width: "95%",
+                                }}
+                                onError={ErrorLoadingImage}
+                            />
+                        </Box>
+                    </Grid>
+                    <Grid xs={7.5}>
+                        <CharacterMaterialGrid character={props.character} />
+                    </Grid>
+                </Grid>
+
+            </CardContent>
+        </Card>
+    )
+
+}
+
+export default CharacterCard
+
+const GetRarityColor = (rarity: number) => {
+    if (rarity === 5) {
+        return "rgb(255, 208, 112)"
+    }
+    if (rarity === 4) {
+        return "rgb(175, 134, 255)"
+    }
+}
+
+const GetBackgroundColor = (rarity: number, opacity = 0.45) => {
+    if (rarity === 5) {
+        return `rgba(255, 199, 129, ${opacity})`;
+    }
+    if (rarity === 4) {
+        return `rgba(193, 153, 253, ${opacity})`;
+    }
+    if (rarity === 3) {
+        return `rgba(115, 176, 244, ${opacity})`;
+    }
+}
