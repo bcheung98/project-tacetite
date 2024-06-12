ea7ce2fe3b4df2c064fb5b57797896d7b280292b
Brian <briantcheung@gmail.com>
2024-06-12 02:20:37 -0700

    Added icons and temp links to nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,3 @@
-import * as React from "react"
-
 // MUI imports
 import { useTheme } from "@mui/material/styles"
 import { AppBar, Box, Toolbar, Typography, Container, ButtonBase, Avatar, CardHeader } from "@mui/material"
@@ -20,7 +18,7 @@ const Nav = () => {
                     <ButtonBase disableRipple href={`/project-tacetite/`}>
                         <CardHeader
                             avatar={
-                                null
+                                <Avatar src={`${process.env.REACT_APP_URL}/elements/Spectro.png`} alt="PROJECT TACETITE" sx={{ height: "64px", width: "64px", backgroundColor: `${theme.appbar.backgroundColor}` }} />
                             }
                             title={
                                 <Typography variant="h6"
@@ -41,7 +39,7 @@ const Nav = () => {
                         <ButtonBase disableRipple href={`/project-tacetite/characters`}>
                             <CardHeader
                                 avatar={
-                                    null
+                                    <Avatar src={(`${process.env.REACT_APP_URL}/icons/Character.png`)} alt="Resonators" sx={{ height: "32px", width: "32px" }} />
                                 }
                                 title={
                                     <Typography variant="body2"
@@ -53,6 +51,44 @@ const Nav = () => {
                                         }}
                                     >
                                         RESONATORS
+                                    </Typography>
+                                }
+                            />
+                        </ButtonBase>
+                        <ButtonBase disableRipple href={`/project-tacetite/weapons`}>
+                            <CardHeader
+                                avatar={
+                                    <Avatar src={(`${process.env.REACT_APP_URL}/icons/Weapon.png`)} alt="Weapons" sx={{ height: "32px", width: "32px" }} />
+                                }
+                                title={
+                                    <Typography variant="body2"
+                                        sx={{
+                                            letterSpacing: ".1rem",
+                                            color: `${theme.text.color}`,
+                                            textDecoration: "none",
+                                            fontWeight: "500",
+                                        }}
+                                    >
+                                        WEAPONS
+                                    </Typography>
+                                }
+                            />
+                        </ButtonBase>
+                        <ButtonBase disableRipple href={`/project-tacetite/echoes`}>
+                            <CardHeader
+                                avatar={
+                                    <Avatar src={(`${process.env.REACT_APP_URL}/icons/Echo.png`)} alt="Echoes" sx={{ height: "32px", width: "32px" }} />
+                                }
+                                title={
+                                    <Typography variant="body2"
+                                        sx={{
+                                            letterSpacing: ".1rem",
+                                            color: `${theme.text.color}`,
+                                            textDecoration: "none",
+                                            fontWeight: "500",
+                                        }}
+                                    >
+                                        ECHOES
                                     </Typography>
                                 }
                             />
