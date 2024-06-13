6483642cfe38e9f29dab259341d69592e777d513
Brian <briantcheung@gmail.com>
2024-06-13 16:27:11 -0700

    Added custom tabs

---
+++ src/helpers/CustomTabs.tsx
@@ -0,0 +1,58 @@
+import { styled } from "@mui/material/styles"
+import { Typography, Tabs, Tab, Box } from "@mui/material"
+
+export const TabPanel = (props: any) => {
+
+    const { children, value, index, ...other } = props
+    return (
+        <div
+            hidden={value !== index}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography component="span">{children}</Typography>
+                </Box>
+            )}
+        </div>
+    )
+}
+
+interface CustomTabsProps {
+    children?: React.ReactNode,
+    value: number,
+    onChange: (event: React.SyntheticEvent, newValue: number) => void
+}
+
+export const StyledTabs = styled((props: CustomTabsProps) => (
+    <Tabs
+        {...props}
+        TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+    />
+))({
+    "& .MuiTabs-indicator": {
+        display: "flex",
+        justifyContent: "center",
+        backgroundColor: "transparent",
+    },
+    "& .MuiTabs-indicatorSpan": {
+        width: "100%",
+        backgroundColor: "rgb(202, 166, 112)",
+    },
+})
+
+interface CustomTabProps {
+    label: string
+}
+
+export const StyledTab = styled((props: CustomTabProps) =>
+    <Tab disableRipple {...props} />
+)(
+    ({ theme }) => ({
+        color: `${theme.text.color}`,
+        fontWeight: "bold",
+        "&.Mui-selected": {
+            color: `${theme.text.highlight}`,
+        },
+    }),
+)
