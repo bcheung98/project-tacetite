d4d8d9946458ce3677ef3214f2c2d71d732ca7f9
Brian <briantcheung@gmail.com>
2024-06-13 01:32:30 -0700

    Added footbar

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ import CharacterPage from "./components/characters/page/_CharacterPage"
 // MUI imports
 import { defaultTheme as theme } from "./theme"
 import { ThemeProvider } from "@mui/material/styles"
+import { AppBar, Typography, Box, IconButton } from "@mui/material"
+import GitHubIcon from "@mui/icons-material/GitHub"
 
 // Type imports
 import { RootState } from "./redux/store"
@@ -40,6 +42,21 @@ const App = (props: any) => {
                     <Route exact path="/characters" component={CharacterBrowser} />
                     <Route path="/character/:char_name" children={<CharacterPage />} />
                 </Switch>
+                <AppBar position="static" sx={{
+                    mt: 10,
+                    mb: -5,
+                    pt: 2,
+                    textAlign: "center",
+                    backgroundColor: `${theme.appbar.backgroundColor}`,
+                    borderTop: `1px solid ${theme.border.color}`,
+                }}>
+                    <Typography sx={{ fontWeight: "bold", mb: "5px" }} variant="body2">Project Tacetite is not affiliated with Kuro Games.<br />Wuthering Waves, images and data are registered trademarks of Kuro Games.</Typography>
+                    <Box>
+                        <IconButton disableRipple href={"https://github.com/bcheung98/project-tacetite"} target="_blank" color="inherit">
+                            <GitHubIcon />
+                        </IconButton>
+                    </Box>
+                </AppBar>
             </Router>
         </ThemeProvider>
     )
