38568bd8efe1ec522a9ea793bcf0976fec7b0608
Brian <briantcheung@gmail.com>
2024-06-15 17:22:41 -0700

    Added materials list

---
+++ src/helpers/MaterialList.js
@@ -0,0 +1,15 @@
+// MAKE SURE TO UPDATE THESE WHEN NEW DROPS/ENEMIES ARE ADDED!!!
+
+export const CharXPMats = ["Character1", "Character2", "Character3", "Character4"]
+export const WeaponXPMats = ["Weapon1", "Weapon2", "Weapon3", "Weapon4"]
+export const EchoXPMats = ["Echo1", "Echo2", "Echo3", "Echo4"]
+
+export const ForgeryMats = ["Cadence1", "Cadence2", "Cadence3", "Cadence4", "Helix1", "Helix2", "Helix3", "Helix4", "Metallic Drip1", "Metallic Drip2", "Metallic Drip3", "Metallic Drip4", "Phlogiston1", "Phlogiston2", "Phlogiston3", "Phlogiston4", "Waveworn1", "Waveworn2", "Waveworn3", "Waveworn4"]
+
+export const CommonMats = ["Howler Core1", "Howler Core2", "Howler Core3", "Howler Core4", "Mask1", "Mask2", "Mask3", "Mask4", "Ring1", "Ring2", "Ring3", "Ring4", "Whisperin Core1", "Whisperin Core2", "Whisperin Core3", "Whisperin Core4"]
+
+export const AscensionMats = ["Belle Poppy", "Coriolus", "Iris", "Lanternberry", "Pavo Plum", "Pecok Flower", "Terraspawn Fungus", "Violet Coral", "Wintry Bell"]
+
+export const BossMats = ["Elegy Tacet Core", "Gold-Dissolving Feather", "Group Abomination Tacet Core", "Hidden Thunder Tacet Core", "Mysterious Code", "Rage Tacet Code", "Roaring Rock Fist", "Sound-Keeping Tacet Core", "Strife Tacet Core", "Thundering Tacet Core"]
+
+export const WeeklyBossMats = ["Dreamless Feather", "Monument Bell", "Unending Destruction"]
