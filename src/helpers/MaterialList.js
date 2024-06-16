6a9218ad3013f3db203d153cbb5628f80fcc21f3
Brian <briantcheung@gmail.com>
2024-06-15 17:51:20 -0700

    Fixed typo

--- src/helpers/MaterialList.js
+++ src/helpers/MaterialList.js
@@ -6,10 +6,10 @@ export const EchoXPMats = ["Echo1", "Echo2", "Echo3", "Echo4"]
 
 export const ForgeryMats = ["Cadence", "Helix", "Metallic Drip", "Phlogiston", "Waveworn"]
 
-export const CommonMats = ["Howler Core", "Mask", "Ring", "Whisperin Core"]
+export const CommonMats = ["Howler Core", "Ring", "Whisperin Core"]
 
 export const AscensionMats = ["Belle Poppy", "Coriolus", "Iris", "Lanternberry", "Pavo Plum", "Pecok Flower", "Terraspawn Fungus", "Violet Coral", "Wintry Bell"]
 
-export const BossMats = ["Elegy Tacet Core", "Gold-Dissolving Feather", "Group Abomination Tacet Core", "Hidden Thunder Tacet Core", "Mysterious Code", "Rage Tacet Code", "Roaring Rock Fist", "Sound-Keeping Tacet Core", "Strife Tacet Core", "Thundering Tacet Core"]
+export const BossMats = ["Elegy Tacet Core", "Gold-Dissolving Feather", "Group Abomination Tacet Core", "Hidden Thunder Tacet Core", "Mysterious Code", "Rage Tacet Core", "Roaring Rock Fist", "Sound-Keeping Tacet Core", "Strife Tacet Core", "Thundering Tacet Core"]
 
 export const WeeklyBossMats = ["Dreamless Feather", "Monument Bell", "Unending Destruction"]
