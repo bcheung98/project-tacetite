61dff065c2e47ee262b5d49d77faa3222753b58b
Brian <briantcheung@gmail.com>
2024-06-15 17:34:52 -0700

    Added materials list

--- src/helpers/MaterialList.js
+++ src/helpers/MaterialList.js
@@ -4,9 +4,9 @@ export const CharXPMats = ["Character1", "Character2", "Character3", "Character4
 export const WeaponXPMats = ["Weapon1", "Weapon2", "Weapon3", "Weapon4"]
 export const EchoXPMats = ["Echo1", "Echo2", "Echo3", "Echo4"]
 
-export const ForgeryMats = ["Cadence1", "Cadence2", "Cadence3", "Cadence4", "Helix1", "Helix2", "Helix3", "Helix4", "Metallic Drip1", "Metallic Drip2", "Metallic Drip3", "Metallic Drip4", "Phlogiston1", "Phlogiston2", "Phlogiston3", "Phlogiston4", "Waveworn1", "Waveworn2", "Waveworn3", "Waveworn4"]
+export const ForgeryMats = ["Cadence", "Helix", "Metallic Drip", "Phlogiston", "Waveworn"]
 
-export const CommonMats = ["Howler Core1", "Howler Core2", "Howler Core3", "Howler Core4", "Mask1", "Mask2", "Mask3", "Mask4", "Ring1", "Ring2", "Ring3", "Ring4", "Whisperin Core1", "Whisperin Core2", "Whisperin Core3", "Whisperin Core4"]
+export const CommonMats = ["Howler Core", "Mask", "Ring", "Whisperin Core"]
 
 export const AscensionMats = ["Belle Poppy", "Coriolus", "Iris", "Lanternberry", "Pavo Plum", "Pecok Flower", "Terraspawn Fungus", "Violet Coral", "Wintry Bell"]
 
