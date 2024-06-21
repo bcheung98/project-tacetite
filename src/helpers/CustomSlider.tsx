a9f11fbaee48d98d25fb8c52fd51778e1ccfe7fc
Brian <briantcheung@gmail.com>
2024-06-20 17:02:34 -0700

    Added custom slider

---
+++ src/helpers/CustomSlider.tsx
@@ -0,0 +1,23 @@
+import { styled } from "@mui/material/styles"
+import { Slider } from "@mui/material"
+import { SwitchColor } from "./ElementColors"
+
+interface CustomSliderProps {
+    element: string
+}
+
+export const CustomSlider = styled(Slider)<CustomSliderProps>(({ element }) => ({
+    color: `${SwitchColor(element)}`,
+    height: 5,
+    "& .MuiSlider-track": {
+        border: "none",
+    },
+    "& .MuiSlider-thumb": {
+        "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+            boxShadow: "inherit",
+        },
+        "&:before": {
+            display: "none",
+        },
+    }
+}))
