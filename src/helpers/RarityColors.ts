08bae355096b547dace987b7d5b8040eec980a82
Brian <briantcheung@gmail.com>
2024-06-23 02:57:18 -0700

    Modified colors

--- src/helpers/RarityColors.ts
+++ src/helpers/RarityColors.ts
@@ -5,6 +5,15 @@ export const GetRarityColor = (rarity: number) => {
     if (rarity === 4) {
         return "rgb(175, 134, 255)"
     }
+    if (rarity === 3) {
+        return "rgb(105, 157, 237)"
+    }
+    if (rarity === 2) {
+        return "rgb(104, 211, 145)"
+    }
+    if (rarity === 1) {
+        return "rgb(175, 175, 175)"
+    }
 }
 
 export const GetBackgroundColor = (rarity: number, opacity = 0.45) => {
@@ -16,5 +25,11 @@ export const GetBackgroundColor = (rarity: number, opacity = 0.45) => {
     }
     if (rarity === 3) {
         return `rgba(115, 176, 244, ${opacity})`;
+    }
+    if (rarity === 2) {
+        return `rgba(114, 169, 156, ${opacity})`;
+    }
+    if (rarity === 1) {
+        return `rgba(195, 195, 195, ${opacity})`;
     }
 }
