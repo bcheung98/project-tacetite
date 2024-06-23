f5c5ccdacd992d30b4d4027563971ac275ac4d3c
Brian <briantcheung@gmail.com>
2024-06-23 01:49:48 -0700

    Added material costs for intro skill

--- src/redux/reducers/AscensionPlannerReducer.ts
+++ src/redux/reducers/AscensionPlannerReducer.ts
@@ -84,54 +84,60 @@ export const PlannerSlice = createSlice({
         },
         updateCharacterCosts: (state, action: PayloadAction<[string, string, {}]>) => {
             let indexChar = state.characterCosts.indexOf((state.characterCosts.find((char: any) => char.name === action.payload[0])) as never)
-            if (indexChar !== -1) {
-                Object.keys(action.payload[2] as {}).forEach((key: string) => {
-                    switch (action.payload[1]) {
-                        case "level":
-                            state.characterCosts[indexChar].costs[key as keyof {}][0] = action.payload[2][key as keyof {}]
-                            break
-                        case "attack":
-                            state.characterCosts[indexChar].costs[key as keyof {}][1][0] = action.payload[2][key as keyof {}]
-                            break
-                        case "attack_node1":
-                            state.characterCosts[indexChar].costs[key as keyof {}][1][1] = action.payload[2][key as keyof {}]
-                            break
-                        case "attack_node2":
-                            state.characterCosts[indexChar].costs[key as keyof {}][1][2] = action.payload[2][key as keyof {}]
-                            break
-                        case "skill":
-                            state.characterCosts[indexChar].costs[key as keyof {}][2][0] = action.payload[2][key as keyof {}]
-                            break
-                        case "skill_node1":
-                            state.characterCosts[indexChar].costs[key as keyof {}][2][1] = action.payload[2][key as keyof {}]
-                            break
-                        case "skill_node2":
-                            state.characterCosts[indexChar].costs[key as keyof {}][2][2] = action.payload[2][key as keyof {}]
-                            break
-                        case "ultimate":
-                            state.characterCosts[indexChar].costs[key as keyof {}][3][0] = action.payload[2][key as keyof {}]
-                            break
-                        case "ultimate_node1":
-                            state.characterCosts[indexChar].costs[key as keyof {}][3][1] = action.payload[2][key as keyof {}]
-                            break
-                        case "ultimate_node2":
-                            state.characterCosts[indexChar].costs[key as keyof {}][3][2] = action.payload[2][key as keyof {}]
-                            break
-                        case "circuit":
-                            state.characterCosts[indexChar].costs[key as keyof {}][4][0] = action.payload[2][key as keyof {}]
-                            break
-                        case "circuit_node1":
-                            state.characterCosts[indexChar].costs[key as keyof {}][4][1] = action.payload[2][key as keyof {}]
-                            break
-                        case "circuit_node2":
-                            state.characterCosts[indexChar].costs[key as keyof {}][4][2] = action.payload[2][key as keyof {}]
-                            break
-                        default:
-                            break
-                    }
-                })
-
-            }
+            Object.keys(action.payload[2] as {}).forEach((key: string) => {
+                switch (action.payload[1]) {
+                    case "level":
+                        state.characterCosts[indexChar].costs[key as keyof {}][0] = action.payload[2][key as keyof {}]
+                        break
+                    case "attack":
+                        state.characterCosts[indexChar].costs[key as keyof {}][1][0] = action.payload[2][key as keyof {}]
+                        break
+                    case "attack_node1":
+                        state.characterCosts[indexChar].costs[key as keyof {}][1][1] = action.payload[2][key as keyof {}]
+                        break
+                    case "attack_node2":
+                        state.characterCosts[indexChar].costs[key as keyof {}][1][2] = action.payload[2][key as keyof {}]
+                        break
+                    case "skill":
+                        state.characterCosts[indexChar].costs[key as keyof {}][2][0] = action.payload[2][key as keyof {}]
+                        break
+                    case "skill_node1":
+                        state.characterCosts[indexChar].costs[key as keyof {}][2][1] = action.payload[2][key as keyof {}]
+                        break
+                    case "skill_node2":
+                        state.characterCosts[indexChar].costs[key as keyof {}][2][2] = action.payload[2][key as keyof {}]
+                        break
+                    case "ultimate":
+                        state.characterCosts[indexChar].costs[key as keyof {}][3][0] = action.payload[2][key as keyof {}]
+                        break
+                    case "ultimate_node1":
+                        state.characterCosts[indexChar].costs[key as keyof {}][3][1] = action.payload[2][key as keyof {}]
+                        break
+                    case "ultimate_node2":
+                        state.characterCosts[indexChar].costs[key as keyof {}][3][2] = action.payload[2][key as keyof {}]
+                        break
+                    case "circuit":
+                        state.characterCosts[indexChar].costs[key as keyof {}][4][0] = action.payload[2][key as keyof {}]
+                        break
+                    case "circuit_node1":
+                        state.characterCosts[indexChar].costs[key as keyof {}][4][1] = action.payload[2][key as keyof {}]
+                        break
+                    case "circuit_node2":
+                        state.characterCosts[indexChar].costs[key as keyof {}][4][2] = action.payload[2][key as keyof {}]
+                        break
+                    case "intro":
+                        state.characterCosts[indexChar].costs[key as keyof {}][5][0] = action.payload[2][key as keyof {}]
+                        break
+                    case "intro_node1":
+                        state.characterCosts[indexChar].costs[key as keyof {}][5][1] = action.payload[2][key as keyof {}]
+                        break
+                    case "intro_node2":
+                        state.characterCosts[indexChar].costs[key as keyof {}][5][2] = action.payload[2][key as keyof {}]
+                        break
+                    default:
+                        break
+                }
+            })
         }
     }
 })
