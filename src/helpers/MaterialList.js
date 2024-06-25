54f81b16618b4552f081f8d8cbb658dbd7620874
Brian <briantcheung@gmail.com>
2024-06-25 15:45:11 -0700

    Changed material order

--- src/helpers/MaterialList.js
+++ src/helpers/MaterialList.js
@@ -4,7 +4,7 @@ export const CharXPMats = ["Character1", "Character2", "Character3", "Character4
 export const WeaponXPMats = ["Weapon1", "Weapon2", "Weapon3", "Weapon4"]
 export const EchoXPMats = ["Echo1", "Echo2", "Echo3", "Echo4"]
 
-export const ForgeryMats = ["Cadence", "Helix", "Metallic Drip", "Phlogiston", "Waveworn"]
+export const ForgeryMats = ["Waveworn", "Cadence", "Phlogiston", "Helix", "Metallic Drip"]
 
 export const CommonMats = ["Howler Core", "Ring", "Whisperin Core"]
 
