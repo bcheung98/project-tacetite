cc973472b48bd0085e58465bea7f200cfe84b482
Brian <briantcheung@gmail.com>
2024-06-26 23:57:42 -0700

    Code cleanup

--- src/helpers/CustomToggleButton.tsx
+++ src/helpers/CustomToggleButton.tsx
@@ -1,5 +1,5 @@
-import { styled } from "@mui/material/styles";
-import { ToggleButton } from "@mui/material";
+import { styled } from "@mui/material/styles"
+import { ToggleButton } from "@mui/material"
 
 export const CustomToggleButton = styled(ToggleButton)(({ theme }) => ({
     "&.MuiToggleButton-root": {
