6fe3deaafe796ec19ded7eb5ff35c2cf4212787a
Brian <briantcheung@gmail.com>
2024-06-28 02:43:01 -0700

    Added new materials

--- src/helpers/MaterialList.js
+++ src/helpers/MaterialList.js
@@ -8,7 +8,7 @@ export const ForgeryMats = ["Waveworn", "Cadence", "Phlogiston", "Helix", "Metal
 
 export const CommonMats = ["Howler Core", "Ring", "Whisperin Core"]
 
-export const AscensionMats = ["Belle Poppy", "Coriolus", "Iris", "Lanternberry", "Pavo Plum", "Pecok Flower", "Terraspawn Fungus", "Violet Coral", "Wintry Bell"]
+export const AscensionMats = ["Belle Poppy", "Coriolus", "Iris", "Lanternberry", "Pecok Flower", "Terraspawn Fungus", "Violet Coral", "Wintry Bell", "Loong's Pearl", "Pavo Plum"]
 
 export const BossMats = ["Elegy Tacet Core", "Gold-Dissolving Feather", "Group Abomination Tacet Core", "Hidden Thunder Tacet Core", "Mysterious Code", "Rage Tacet Core", "Roaring Rock Fist", "Sound-Keeping Tacet Core", "Strife Tacet Core", "Thundering Tacet Core"]
 
