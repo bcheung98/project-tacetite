da668cc67e221f36b7bc3d221d38a06f646e3787
Brian <briantcheung@gmail.com>
2024-06-29 13:33:56 -0700

    Reordered tags and updated descriptions

--- src/helpers/CharacterTags.js
+++ src/helpers/CharacterTags.js
@@ -1,90 +1,98 @@
 export const Tags = {
-    "Aero DMG Amplification": {
-        "description": "Provides Aero DMG Amplification for a specific teammate",
-        "color": "#ff7777",
+    "Support and Healer": {
+        "description": "Improve team survivability",
+        "color": "#77ffb7",
     },
-    "Basic Attack DMG Amplification": {
-        "description": "Provides Basic Attack DMG Amplification for a specific teammate",
-        "color": "#ff7777",
-    },
-    "Basic Attack Damage": {
-        "description": "Deal higher Basic Attack Damage",
-        "color": "#ffde73",
+    "Main Damage Dealer": {
+        "description": "Powerful damage dealer",
+        "color": "#ff8441",
     },
     "Concerto Efficiency": {
         "description": "Quick concerto energy regeneration",
         "color": "#ff4040",
     },
-    "Coordinated Attack": {
-        "description": "Trigger Coordinated Attack under specific circumstances",
-        "color": "#77adff",
-    },
-    "DMG Amplification": {
-        "description": "Provides DMG Amplification for a specific teammate",
-        "color": "#ff7777",
-    },
-    "Electro DMG Amplification": {
-        "description": "Provides Electro DMG Amplification for a specific teammate",
-        "color": "#ff7777",
-    },
-    "Fusion DMG Amplification": {
-        "description": "Provides Fusion DMG Amplification for a specific teammate",
-        "color": "#ff7777",
-    },
-    "Havoc DMG Amplification": {
-        "description": "Provides Havoc DMG Amplification for a specific teammate",
-        "color": "#ff7777",
-    },
-    "Heavy Attack DMG Amplification": {
-        "description": "Provides Heavy Attack DMG Amplification for a specific teammate",
-        "color": "#ff7777",
+    "Basic Attack Damage": {
+        "description": "Deal higher Basic Attack DMG",
+        "color": "#ffde73",
     },
     "Heavy Attack Damage": {
-        "description": "Deal higher Heavy Attack Damage",
+        "description": "Deal higher Heavy Attack DMG",
         "color": "#ffde73",
-    },
-    "Interruption Resistance Boost": {
-        "description": "Provides interruption resistance of a specific teammate",
-        "color": "#77adff",
-    },
-    "Main Damage Dealer": {
-        "description": "Deal continuous Damage",
-        "color": "#ff8441",
-    },
-    "Resonance Liberation Regeneration": {
-        "description": "Restore the Resonance Energy of a specific teammate",
-        "color": "#77adff",
-    },
-    "Resonance Liberation DMG Amplification": {
-        "description": "Provides Resonance Liberation DMG Amplification for a specific teammate",
-        "color": "#ff7777",
-    },
-    "Resonance Liberation Damage": {
-        "description": "Deal higher Resonance Liberation Damage",
-        "color": "#ffde73",
-    },
-    "Resonance Skill DMG Amplification": {
-        "description": "Provides Resonance Skill DMG Amplification for a specific teammate",
-        "color": "#ff7777",
     },
     "Resonance Skill Damage": {
-        "description": "Deal higher Resonance Skill Damage",
+        "description": "Deal higher Resonance Skill DMG",
         "color": "#ffde73",
+    },
+    "Resonance Liberation Damage": {
+        "description": "Deal higher Resonance Liberation DMG",
+        "color": "#ffde73",
+    },
+    "Traction": {
+        "description": "Pull targets in range towards a specific position",
+        "color": "#77adff",
+    },
+    "Coordinated Attack": {
+        "description": "Excel at Coordinated Attacks",
+        "color": "#77adff",
     },
     "Stagnation": {
         "description": "Slow down targets within a certain range",
         "color": "#77adff",
     },
-    "Support and Healer": {
-        "description": "Improve team survival",
-        "color": "#77ffb7",
-    },
-    "Traction": {
-        "description": "Pull targets in range towards a specific position",
+    "Resonance Liberation Regeneration": {
+        "description": "Restore the Resonance Energy of a specific teammate",
         "color": "#77adff",
     },
     "Vibration Strength Reduction": {
         "description": "Reduce the Vibration Strength of targets more effectively",
         "color": "#77adff",
     },
+    "DMG Amplification": {
+        "description": "Provides DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Glacio DMG Amplification": {
+        "description": "Provides Glacio DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Fusion DMG Amplification": {
+        "description": "Provides Fusion DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Electro DMG Amplification": {
+        "description": "Provides Electro DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Aero DMG Amplification": {
+        "description": "Provides Aero DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Spectro DMG Amplification": {
+        "description": "Provides Spectro DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Havoc DMG Amplification": {
+        "description": "Provides Havoc DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Basic Attack DMG Amplification": {
+        "description": "Provides Basic Attack DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Heavy Attack DMG Amplification": {
+        "description": "Provides Heavy Attack DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Resonance Skill DMG Amplification": {
+        "description": "Provides Resonance Skill DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Resonance Liberation DMG Amplification": {
+        "description": "Provides Resonance Liberation DMG Amplification for a specific teammate",
+        "color": "#ff7777",
+    },
+    "Interruption Resistance Boost": {
+        "description": "Boost the interruption resistance of a specific teammate",
+        "color": "#77adff",
+    }
 }
