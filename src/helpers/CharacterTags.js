a7e67aa886d47f8c2e2392cd30d0beafd9422169
Brian <briantcheung@gmail.com>
2024-06-28 20:16:40 -0700

    Renamed tags

--- src/helpers/CharacterTags.js
+++ src/helpers/CharacterTags.js
@@ -1,79 +1,79 @@
 export const Tags = {
-    "Aero Damage Boost": {
-        "description": "Boost the Aero Damage of a specific teammate",
+    "Aero DMG Amplification": {
+        "description": "Provides Aero DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
-    "Basic Attack Damage Boost": {
-        "description": "Boost the Basic Attack Damage of a specific teammate",
+    "Basic Attack DMG Amplification": {
+        "description": "Provides Basic Attack DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
     "Basic Attack Damage": {
         "description": "Deal higher Basic Attack Damage",
         "color": "#ffde73",
     },
-    "Burst Damage Dealer": {
-        "description": "Deal massive Damage instantly",
+    "Concerto Efficiency": {
+        "description": "Quick concerto energy regeneration",
         "color": "#ff4040",
     },
     "Coordinated Attack": {
         "description": "Trigger Coordinated Attack under specific circumstances",
         "color": "#77adff",
     },
-    "Damage Boost": {
-        "description": "Boost the Damage of a specific teammate",
+    "DMG Amplification": {
+        "description": "Provides DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
-    "Deceleration": {
-        "description": "Slow down targets within a certain range",
-        "color": "#77adff",
-    },
-    "Electro Damage Boost": {
-        "description": "Boost the Electro Damage of a specific teammate",
+    "Electro DMG Amplification": {
+        "description": "Provides Electro DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
-    "Fusion Damage Boost": {
-        "description": "Boost the Fusion Damage of a specific teammate",
+    "Fusion DMG Amplification": {
+        "description": "Provides Fusion DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
-    "Havoc Damage Boost": {
-        "description": "Boost the Havoc Damage of a specific teammate",
+    "Havoc DMG Amplification": {
+        "description": "Provides Havoc DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
-    "Heavy Attack Damage Boost": {
-        "description": "Boost the Heavy Attack Damage of a specific teammate",
+    "Heavy Attack DMG Amplification": {
+        "description": "Provides Heavy Attack DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
     "Heavy Attack Damage": {
         "description": "Deal higher Heavy Attack Damage",
         "color": "#ffde73",
     },
-    "Knockback Resistance Boost": {
-        "description": "Boost the Knockback Resistance of a specific teammate",
+    "Interruption Resistance Boost": {
+        "description": "Provides interruption resistance of a specific teammate",
         "color": "#77adff",
     },
     "Main Damage Dealer": {
         "description": "Deal continuous Damage",
         "color": "#ff8441",
     },
-    "Resonance Liberation Charge": {
+    "Resonance Liberation Regeneration": {
         "description": "Restore the Resonance Energy of a specific teammate",
         "color": "#77adff",
     },
-    "Resonance Liberation Damage Boost": {
-        "description": "Boost the Resonance Liberation Damage of a specific teammate",
+    "Resonance Liberation DMG Amplification": {
+        "description": "Provides Resonance Liberation DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
     "Resonance Liberation Damage": {
         "description": "Deal higher Resonance Liberation Damage",
         "color": "#ffde73",
     },
-    "Resonance Skill Damage Boost": {
-        "description": "Boost the Resonance Skill Damage of a specific teammate",
+    "Resonance Skill DMG Amplification": {
+        "description": "Provides Resonance Skill DMG Amplification for a specific teammate",
         "color": "#ff7777",
     },
     "Resonance Skill Damage": {
         "description": "Deal higher Resonance Skill Damage",
         "color": "#ffde73",
+    },
+    "Stagnation": {
+        "description": "Slow down targets within a certain range",
+        "color": "#77adff",
     },
     "Support and Healer": {
         "description": "Improve team survival",
