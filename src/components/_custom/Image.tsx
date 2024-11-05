c6434910f567dbe56f1b2c9f2221ce20bef42828
Brian <briantcheung@gmail.com>
2024-11-04 22:04:59 -0800

    Added custom components

---
+++ src/components/_custom/Image.tsx
@@ -0,0 +1,69 @@
+import React from "react"
+
+// Component imports
+import { CustomTooltip } from "../_styled/StyledTooltip"
+
+// MUI imports
+import { TooltipProps } from "@mui/material"
+
+const defaultImageStyle: React.CSSProperties = {
+    width: "auto",
+    height: "auto",
+    boxSizing: "border-box"
+}
+
+interface ImageProps {
+    src: string,
+    fallbackSrc?: string,
+    alt?: string,
+    id?: string,
+    loading?: "eager" | "lazy",
+    style?: React.CSSProperties,
+    tooltip?: {
+        title: string,
+        placement?: TooltipProps["placement"]
+    }
+}
+
+function Image({
+    src,
+    fallbackSrc = "images/Unknown",
+    alt = "",
+    id = src,
+    loading = "lazy",
+    style,
+    tooltip
+}: ImageProps) {
+
+    const imgStyle = Object.assign({...defaultImageStyle}, style)
+
+    let tooltipTitle, tooltipPlacement
+    if (tooltip !== undefined) {
+        tooltipTitle = tooltip.title
+        if (tooltip.placement !== undefined) {
+            tooltipPlacement = tooltip.placement
+        }
+        else {
+            tooltipPlacement = "top" as TooltipProps["placement"]
+        }
+    }
+
+    return (
+        <CustomTooltip title={tooltipTitle} arrow placement={tooltipPlacement}>
+            <img
+                src={`${process.env.REACT_APP_URL}/${src.split(" ").join("_")}.png`}
+                alt={alt}
+                id={id}
+                loading={loading}
+                style={imgStyle}
+                onError={(e: any) => {
+                    e.target.src = `${process.env.REACT_APP_URL}/${fallbackSrc}.png`
+                    e.onError = null
+                }}
+            />
+        </CustomTooltip>
+    )
+
+}
+
+export default Image
