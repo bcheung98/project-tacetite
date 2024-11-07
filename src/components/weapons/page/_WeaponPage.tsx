092df148d219267a033086e1ab948485d4827e09
Brian <briantcheung@gmail.com>
2024-11-06 19:40:36 -0800

    Styling adjustments

--- src/components/weapons/page/_WeaponPage.tsx
+++ src/components/weapons/page/_WeaponPage.tsx
@@ -6,17 +6,16 @@ import parse from "html-react-parser"
 // Component imports
 import WeaponStatsTable from "./WeaponStatsTable"
 import WeaponAscension from "./WeaponAscension"
-import { CustomTooltip } from "../../_styled/StyledTooltip"
+import Image from "../../_custom/Image"
 import { CustomSlider } from "../../_styled/StyledSlider"
 import { TabPanel, StyledTabs, StyledTab } from "../../_styled/StyledTabs"
 
 // MUI imports
-import { useTheme, Typography, Box, Avatar, AppBar } from "@mui/material"
+import { useTheme, useMediaQuery, Typography, Box, AppBar } from "@mui/material"
 import Grid from "@mui/material/Grid2"
 
 // Helper imports
 import { GetBackgroundColor, GetRarityColor } from "../../../helpers/RarityColors"
-import ErrorLoadingImage from "../../../helpers/ErrorLoadingImage"
 
 // Type imports
 import { RootState } from "../../../redux/store"
@@ -25,6 +24,8 @@ import { Weapon } from "../../../types/weapon"
 function WeaponPage() {
 
     const theme = useTheme()
+
+    const matches = useMediaQuery(theme.breakpoints.up("sm"))
 
     const { wep_name } = useParams<{ wep_name: string }>()
     const weapons = useSelector((state: RootState) => state.weapons.weapons)
@@ -63,8 +64,8 @@ function WeaponPage() {
             <React.Fragment>
                 <Grid container spacing={3}>
                     <Grid size="auto">
-                        <img
-                            src={(`${process.env.REACT_APP_URL}/weapons/${name.split(" ").join("_")}.png`)}
+                        <Image
+                            src={`weapons/${name}`}
                             alt={name}
                             style={{
                                 border: `2px solid ${GetRarityColor(rarity)}`,
@@ -73,48 +74,55 @@ function WeaponPage() {
                                 backgroundSize: "100%",
                                 boxShadow: `inset 0 0 50px 20px ${GetBackgroundColor(rarity)}`,
                             }}
-                            onError={ErrorLoadingImage} />
+                        />
                     </Grid>
-                    <Grid size="grow">
+                    <Grid size={{ xs: 12, sm: "grow" }}>
                         <Box
                             sx={{
-                                px: "5px",
-                                py: "15px",
+                                p: "15px",
                                 border: `1px solid ${theme.border.color}`,
                                 borderRadius: "5px",
                                 backgroundColor: `${theme.paper.backgroundColor}`,
                             }}
                         >
                             <Box sx={{ display: "flex", alignItems: "center" }}>
-                                <CustomTooltip title={`${type}`} arrow placement="bottom">
-                                    <Avatar variant="square" src={`${process.env.REACT_APP_URL}/weapons/icons/${type}.png`} alt={type} onError={ErrorLoadingImage} sx={{ marginRight: "-20px", height: "96px", width: "96px", backgroundColor: `${theme.paper.backgroundColor}` }} />
-                                </CustomTooltip>
+                                <Image
+                                    src={`weapons/icons/${type}`}
+                                    alt={type}
+                                    style={{
+                                        marginRight: "-20px",
+                                        marginTop: "auto",
+                                        marginBottom: "auto",
+                                        height: matches ? "96px" : "72px",
+                                        width: matches ? "96px" : "72px",
+                                        backgroundColor: `${theme.paper.backgroundColor}`
+                                    }}
+                                    tooltip={{ title: type }}
+                                />
                                 <Box sx={{ ml: "30px" }}>
                                     <Typography
-                                        variant="h4"
-                                        noWrap
                                         sx={{
-                                            fontWeight: "bold",
+                                            fontFamily: `${theme.font.styled.family}`,
+                                            fontWeight: `${theme.font.styled.weight}`,
+                                            fontSize: { xs: "24px", sm: "32px" },
                                             color: `${theme.text.color}`,
-                                            textAlign: "center",
                                         }}
                                     >
                                         {weapon.displayName ? weapon.displayName : name}
                                     </Typography>
                                     <Box sx={{ mt: "5px" }}>
-                                        <Typography sx={{ color: `${theme.text.star}`, textShadow: "#e3721b 1px 1px 10px", userSelect: "none" }} variant="h4">
+                                        <Typography sx={{ color: `${theme.text.star}`, fontSize: { xs: "24px", sm: "30px" }, textShadow: "#e3721b 1px 1px 10px", userSelect: "none" }}>
                                             {[...Array(rarity).keys()].map(() => "✦")}
                                         </Typography>
                                     </Box>
                                 </Box>
                             </Box>
-                            <hr style={{ border: `0.5px solid ${theme.border.color}`, margin: "15px" }} />
+                            <hr style={{ border: `0.5px solid ${theme.border.color}`, margin: "15px 0 15px 0" }} />
                             <Typography
-                                variant="body1"
                                 sx={{
-                                    m: "15px",
-                                    fontWeight: "500",
-                                    color: `${theme.text.color}`,
+                                    fontFamily: `${theme.font.styled.family}`,
+                                    fontSize: { xs: "13.5px", sm: "16px" },
+                                    m: "5px"
                                 }}
                             >
                                 <i>{description}</i>
@@ -122,8 +130,7 @@ function WeaponPage() {
                         </Box>
                         <Box
                             sx={{
-                                px: "5px",
-                                py: "15px",
+                                p: "15px",
                                 my: "15px",
                                 border: `1px solid ${theme.border.color}`,
                                 borderRadius: "5px",
@@ -131,15 +138,15 @@ function WeaponPage() {
                                 color: `${theme.text.color}`,
                             }}
                         >
-                            <Typography variant="h6" sx={{ fontWeight: "bold", mx: "15px", }}>
+                            <Typography sx={{ fontSize: "20px", fontFamily: `${theme.font.styled.family}` }}>
                                 {stats.passive.name}
                             </Typography>
                             <br />
-                            <Typography variant="body1" sx={{ color: `${theme.text.color}`, mx: "15px" }}>
+                            <Typography sx={{ fontSize: "14.5px" }}>
                                 {parse(stats.passive.description)}
                             </Typography>
-                            <Box sx={{ display: "flex", alignItems: "center", width: "20%", mt: "15px", mx: "15px", }}>
-                                <Typography variant="h6" sx={{ fontWeight: "500", color: `${theme.text.color}`, minWidth: "50px" }}>
+                            <Box sx={{ display: "flex", alignItems: "center", width: "20%", mt: "15px" }}>
+                                <Typography sx={{ fontSize: "18px", fontFamily: `${theme.font.styled.family}`, color: `${theme.text.color}`, minWidth: "50px" }}>
                                     R{sliderValue}
                                 </Typography>
                                 <CustomSlider
@@ -148,8 +155,8 @@ function WeaponPage() {
                                     min={1}
                                     max={maxValue}
                                     onChange={handleSliderChange}
-                                    element=""
-                                    sx={{ minWidth: "100px" }} />
+                                    sx={{ minWidth: "100px", mt: "2.5px" }}
+                                />
                             </Box>
                         </Box>
                         <Box
