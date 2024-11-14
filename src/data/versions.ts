b6633abd691c988dbe519b7a78a1485a66222f63
Brian <briantcheung@gmail.com>
2024-11-13 17:07:06 -0800

    Version 1.4 update

--- src/data/versions.ts
+++ src/data/versions.ts
@@ -1,5 +1,6 @@
 // MAKE SURE TO CHANGE THIS EVERY UPDATE!
 export const updates = [
+    { version: "1.4", name: "When the Night Knocks" },
     { version: "1.3", name: "To The Shore's End" },
     { version: "1.2", name: "In the Turquoise Moonglow" },
     { version: "1.1", name: "Thaw of Eons" },
