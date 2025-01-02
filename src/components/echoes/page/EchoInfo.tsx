a9212e15fb5fa649e59792fb2b7da05cfcd89d92
Brian <briantcheung@gmail.com>
2025-01-01 16:19:31 -0800

    Adjust font size

--- src/components/echoes/page/EchoInfo.tsx
+++ src/components/echoes/page/EchoInfo.tsx
@@ -4,7 +4,7 @@ import { TextStyled } from "styled/StyledTypography";
 import { FlexBox } from "styled/StyledBox";
 
 // MUI imports
-import { useTheme, Box, Divider, Card } from "@mui/material";
+import { useTheme, Box, Divider, Card, Stack } from "@mui/material";
 
 // Helper imports
 import { parseSkillDescription } from "helpers/parseSkillDescription";
@@ -24,25 +24,31 @@ function EchoInfo({ echo }: EchoProps) {
                 backgroundColor: theme.background(2),
             }}
         >
-            <Box sx={{ mb: "8px" }}>
-                <TextStyled variant="h4-styled">{`${displayName} (${code})`}</TextStyled>
-            </Box>
-            <FlexBox sx={{ flexWrap: "wrap", gap: "8px" }}>
-                <InfoChip
-                    color="tertiary"
-                    label={`${echo.class} Class`}
-                    padding="0px"
-                />
-                <InfoChip
-                    color="tertiary"
-                    label={`Cost: ${cost}`}
-                    padding="0px"
-                />
-            </FlexBox>
-            <Divider sx={{ my: "16px" }} />
-            <TextStyled sx={{ fontStyle: "italic" }}>
-                {parseSkillDescription(description)}
-            </TextStyled>
+            <Stack spacing={2} divider={<Divider />}>
+                <Box>
+                    <Box sx={{ mb: "8px" }}>
+                        <TextStyled variant="h4-styled">{`${displayName} (${code})`}</TextStyled>
+                    </Box>
+                    <FlexBox sx={{ flexWrap: "wrap", gap: "8px" }}>
+                        <InfoChip
+                            color="tertiary"
+                            label={`${echo.class} Class`}
+                            padding="0px"
+                        />
+                        <InfoChip
+                            color="tertiary"
+                            label={`Cost: ${cost}`}
+                            padding="0px"
+                        />
+                    </FlexBox>
+                </Box>
+                <TextStyled
+                    variant="subtitle1-styled"
+                    sx={{ fontStyle: "italic" }}
+                >
+                    {parseSkillDescription(description)}
+                </TextStyled>
+            </Stack>
         </Card>
     );
 }
