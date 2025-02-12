8edb81ae2d29b64966679fca530584d6b8ecedd3
Brian <briantcheung@gmail.com>
2025-02-11 17:45:44 -0800

    Version 2.1 Update

--- src/data/versions.ts
+++ src/data/versions.ts
@@ -1,9 +1,10 @@
 // MAKE SURE TO CHANGE THIS EVERY UPDATE!
 export const updates = [
+    { version: "2.1", name: "Waves Sing, and the Cerulean Bird Calls" },
     { version: "2.0", name: "All Silent Souls Can Sing" },
     { version: "1.4", name: "When the Night Knocks" },
     { version: "1.3", name: "To The Shore's End" },
     { version: "1.2", name: "In the Turquoise Moonglow" },
     { version: "1.1", name: "Thaw of Eons" },
-    { version: "1.0", name: "Waking of a World" }
-]
+    { version: "1.0", name: "Waking of a World" },
+];
